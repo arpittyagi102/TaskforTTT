5b463f8e8d6a0e6a83d93008b17b4abb16f38ec5
Arpit <arpittyagirocks@gmail.com>
2023-05-12 15:20:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center mt-5'>This is a fresh new React App <i class="bi-star-fill" style={{ color: "yellow" }}></i></h1>
+    </>
+  );
+}
+
+export default App;
